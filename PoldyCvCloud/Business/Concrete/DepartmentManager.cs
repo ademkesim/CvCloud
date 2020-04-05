5f0dabbb47834b4226babd70e29769b3713625f6
Hasan Hüseyin <hhsonmez68@gmail.com>
2020-04-05 13:25:34 +0300

    Tüm interface ler implement edildi.

---
+++ PoldyCvCloud/Business/Concrete/DepartmentManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+
+namespace Business.Concrete
+{
+   public class DepartmentManager:IDepartmentService
+    {
+        public IResult Add(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Department>> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
