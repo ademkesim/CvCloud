9974f8dca2ad4787d577f7bcd2819d30013cb54b
Hasan Hüseyin <hhsonmez68@gmail.com>
2020-04-05 12:32:58 +0300

    IApplyService Metotları Tanımlandı.

---
+++ PoldyCvCloud/Business/Abstract/IApplyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+  public  interface IApplyService
+  {
+      IDataResult<List<Apply>> GetList();
+      IResult Add(Apply apply);
+
+      IResult Update(Apply apply);
+
+      IResult Delete(Apply apply);
+
+      IDataResult<List<Apply>> GetByDepartment(int getbydepartmentId);
+
+      IDataResult<List<Apply>> GetByTitle(int getbytitle);
+
+  }
+}
