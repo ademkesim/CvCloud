84407875dbc74412b903bddc5d67fda9dbb48027
Hasan Hüseyin <hhsonmez68@gmail.com>
2020-04-05 13:12:00 +0300

    IPersonService Metotları Tanımlandı.

---
+++ PoldyCvCloud/Business/Abstract/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+public interface IPersonService
+{
+    IResult Add(Person person);
+
+    IResult Update(Person person);
+
+    IResult Delete(Person person);
+
+    IDataResult<List<Person>> GetList();
+    }
+}
