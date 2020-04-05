223faed381e0b45eeea030de5dc1f711168fa581
Hasan Hüseyin <hhsonmez68@gmail.com>
2020-04-05 12:52:43 +0300

    IDepartmentService Metotları Tanımlandı.

---
+++ PoldyCvCloud/Business/Abstract/IDepartmentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IDepartmentService
+    {
+        IResult Add(Department department);
+
+        IResult Update(Department department);
+
+        IResult Delete(Department department);
+
+        IDataResult<List<Department>> GetList();
+
+    }
+}
