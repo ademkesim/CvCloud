aff7e483acd2f1bf918e1133636a1dea2315a213
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-04-05 20:03:23 +0300

    DataAccess klasörler eklendi

---
+++ PoldyCvCloud/DataAccess/Concrete/EntityFramework/Contexts/PoldyContexts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    class PoldyContexts:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\MSSQLLocalDB;Database=PoldyCv;Trusted_Connection=true");
+        }
+
+    }
+}
