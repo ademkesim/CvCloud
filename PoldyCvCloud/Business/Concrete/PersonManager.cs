6a7895eb7dd4dc36c3ba87fa260a71d3965b0fa5
Hasan Hüseyin <hhsonmez68@gmail.com>
2020-04-05 14:50:19 +0300

    Tüm interfaces ler tekrardan implement edildi.

---
+++ PoldyCvCloud/Business/Concrete/PersonManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+   public class PersonManager:IPersonService
+    {
+        public IResult Add(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Person>> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
