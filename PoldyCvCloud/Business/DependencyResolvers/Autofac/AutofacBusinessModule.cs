0e997c827368598273f91c8d98944e17ca257843
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-04-06 14:21:06 +0300

    refactoring

---
+++ PoldyCvCloud/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Autofac;
+using Module = Autofac.Module;
+
+namespace Business.DependencyResolvers.Autofac
+{
+   public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplyManager>().As<IApplyService>();
+            builder.RegisterType<CurriculumVitaeManager>().As<ICurriculumVitaeService>();
+            builder.RegisterType<DepartmentManager>().As<IDepartmentService>();
+            builder.RegisterType<JobAdvertisementManager>().As<IJobAdvertisementService>();
+            builder.RegisterType<PersonManager>().As<IPersonService>();
+            builder.RegisterType<TitleManager>().As<ITitleService>();
+
+        }
+    }
+}
