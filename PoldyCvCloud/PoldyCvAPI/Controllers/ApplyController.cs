5c7660df8764ee9f087285ce4eb3eeea65463de9
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-04-07 18:44:15 +0300

    ApplyController eklendi

---
+++ PoldyCvCloud/PoldyCvAPI/Controllers/ApplyController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoldyCvAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplyController : ControllerBase
+    {
+        private IApplyService _applyService;
+        public ApplyController(IApplyService applyService)
+        {
+            _applyService = applyService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result = _applyService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getbydepartment")]
+        public IActionResult GetByDepartment(int departmentId)
+        {
+            var result = _applyService.GetByDepartment(departmentId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getbytitle")]
+        public IActionResult GetByTitle(int titleId)
+        {
+            var result = _applyService.GetByTitle(titleId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Apply apply)
+        {
+            var result = _applyService.Add(apply);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Apply apply)
+        {
+            var result = _applyService.Delete(apply);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Apply apply)
+        {
+            var result = _applyService.Update(apply);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
