634dc494524339c40027a6c8f097a8985060a944
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-04-07 21:18:28 +0300

    refactoring

--- PoldyCvCloud/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ PoldyCvCloud/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -17,22 +17,25 @@ namespace Business.DependencyResolvers.Autofac
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<ApplyManager>().As<IApplyService>();
-            builder.RegisterType<CurriculumVitaeManager>().As<ICurriculumVitaeService>();
-            builder.RegisterType<DepartmentManager>().As<IDepartmentService>();
-            builder.RegisterType<JobAdvertisementManager>().As<IJobAdvertisementService>();
-            builder.RegisterType<PersonManager>().As<IPersonService>();
-            builder.RegisterType<TitleManager>().As<ITitleService>();
-            builder.RegisterType<AutoManager>().As<IAutoService>();
-            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
-            builder.RegisterType<EfPersonDal>().As<IPersonDal>();
             builder.RegisterType<EfApplyDal>().As<IApplyDal>();
+
+            builder.RegisterType<CurriculumVitaeManager>().As<ICurriculumVitaeService>();
             builder.RegisterType<EfCurricilumVitaeDal>().As<ICurriculumVitaeDal>();
+
+            builder.RegisterType<DepartmentManager>().As<IDepartmentService>();
             builder.RegisterType<EfDepartmentDal>().As<IDepartmentDal>();
+
+            builder.RegisterType<JobAdvertisementManager>().As<IJobAdvertisementService>();
             builder.RegisterType<EfJobAdvertisementDal>().As<IJobAdvertisementDal>();
+
+            builder.RegisterType<PersonManager>().As<IPersonService>();
+            builder.RegisterType<EfPersonDal>().As<IPersonDal>();
+
+            builder.RegisterType<TitleManager>().As<ITitleService>();
             builder.RegisterType<EfTitleDal>().As<ITitleDal>();
 
-
-
+            builder.RegisterType<AutoManager>().As<IAutoService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
 
         }
     }
