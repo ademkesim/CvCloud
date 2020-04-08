65895fcbb349c89469236d4887eb5a928528dc70
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-04-08 23:32:00 +0300

    PersonController kodlandı

---
+++ PoldyCvCloud/PoldyCvAPI/Controllers/PersonController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoldyCvAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController:ControllerBase
+    {
+        private IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Person person)
+        {
+            var result = _personService.Add(person);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Person person)
+        {
+            var result = _personService.Update(person);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Person person)
+        {
+            var result = _personService.Delete(person);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result = _personService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
