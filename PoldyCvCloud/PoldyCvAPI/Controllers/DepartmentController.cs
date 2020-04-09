8a7bc55b0a96b53fcc88e372dd3278364e6a7c84
Hasan Hüseyin <hhsonmez68@gmail.com>
2020-04-09 19:08:09 +0300

    Department ve Title Controller Bitti

---
+++ PoldyCvCloud/PoldyCvAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Constans;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoldyCvAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private IDepartmentService _departmentService;
+
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Department department)
+        {
+            var result = _departmentService.Add(department);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Department department)
+        {
+            var result = _departmentService.Update(department);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Department department)
+        {
+            var result = _departmentService.Delete(department);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getlist")]
+        public IActionResult GetList()
+        {
+            var result = _departmentService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
---
+++ PoldyCvCloud/PoldyCvAPI/Controllers/TitleController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using iTextSharp.text.pdf.parser;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoldyCvAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TitleController : ControllerBase
+    {
+        private ITitleService _titleService;
+
+        public TitleController(ITitleService titleService)
+        {
+            _titleService = titleService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Title title)
+        {
+            var result = _titleService.Add(title);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Title title)
+        {
+            var result = _titleService.Update(title);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Title title)
+        {
+            var result = _titleService.Delete(title);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getbydepartment")]
+        public IActionResult GetByDepartment(int id)
+        {
+            var result = _titleService.GetByDepartment(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getlist")]
+        public IActionResult GetList()
+        {
+            var result = _titleService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
